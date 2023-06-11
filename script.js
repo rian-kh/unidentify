eca08ffbd35440e06a9542be75749e6f044269a6
rian-kh <128095876+rkhandk@users.noreply.github.com>
2023-06-11 02:03:08 -0400

    Added element hiding, genre searching by input

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 // Code adapted from https://developer.spotify.com/documentation/web-api/howtos/web-app-profile
 
 
-const clientId = "client-id"; 
 const params = new URLSearchParams(window.location.search);
 
 // Checks if code was found in callback
 const code = params.get("code");
+const user = "user"; 
 const songDiv = document.getElementById('songs');
 const searchDiv = document.getElementById('search');
+const profileDiv = document.getElementById('profile');
 
+// Button function redirects
 document.getElementById("apiButton").onclick = callAPI;
+document.getElementById("searchButton").onclick = outputSongs;
+
+const accessToken = await getAccessToken(user, code);
+const profile = await fetchProfile(accessToken);
+
 
 // Code to run after authorization (Artist finding, etc)
 if (code) {
-    const accessToken = await getAccessToken(clientId, code);
-    const profile = await fetchProfile(accessToken);
-    
-    
 
-
-    
     // Write out top artist w/ genre
     const topInfo = await fetchTop(accessToken);
+
+    // Make profile/search visible
+    document.getElementById('profile').style.display = "inline";
+    document.getElementById('searchBox').style.display = "inline";
+    songDiv.innerHTML = "<h1>Your Artists and Genres:</h1>"
+
+    // Show your top 50 artists with their genres
     for (let i = 0; i < topInfo.items.length; i++){
         let string = `<p>${i+1}. ${topInfo.items[i].name}, Genres: ${topInfo.items[i].genres}</p>`
         songDiv.innerHTML += string
@@ -29,35 +37,49 @@ if (code) {
         
     }
 
-    // Write out a song matching the genre: shoegaze
-    const songInfo = await fetchSong(accessToken);
-    console.log(songInfo)
-
-    for (let i = 0; i < songInfo.tracks.items.length; i++){
-
-        let trackid = songInfo.tracks.items[i].id
-        let string = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${trackid}?utm_source=generator" width="30%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>                <p>&nbsp;</p>`
-        
-        searchDiv.innerHTML += string
-        searchDiv.innerHTML += "</p><p>&nbsp;</p>";
-    }
+  
+  
 
 
     populateUI(profile);
 }
 
-
+// For initial access token request
 function callAPI() {
     if (!code) {
-        redirectToAuthCodeFlow(clientId); 
+        redirectToAuthCodeFlow(user); 
     } 
+}
+
+// Outputs 5 songs matching the genre specified
+async function outputSongs() {
+    var genreInput = document.getElementById("input").value;
+    genreInput = genreInput.replace(/\s/g, "+")
+
+    if(!code)
+        return;
+
+    const songInfo = await fetchSong(accessToken, `https://api.spotify.com/v1/search?q=tag%3Ahipster+genre%3A${genreInput}&type=track&market=US&limit=5`);
+    console.log(songInfo)
+
+
+    searchDiv.innerHTML = `<h1>Searched songs:</h1>\n<p>Genre: ${genreInput}, lowest 10% popularity:</p>`;
+
+    for (let i = 0; i < songInfo.tracks.items.length; i++){
+
+        let trackid = songInfo.tracks.items[i].id
+        let string = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${trackid}?utm_source=generator" height="10%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>                <p>&nbsp;</p>`
+            
+        searchDiv.innerHTML += string
+        searchDiv.innerHTML += "</p><p>&nbsp;</p>";
+    }
+    
 }
 
 
 
-
 // Redirects to Spotify authorization 
-export async function redirectToAuthCodeFlow(clientId) {
+export async function redirectToAuthCodeFlow(user) {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
 
@@ -65,7 +87,7 @@ export async function redirectToAuthCodeFlow(clientId) {
 
     
     const params = new URLSearchParams();
-    params.append("client_id", clientId);
+    params.append("client_id", user);
     params.append("response_type", "code");
     params.append("redirect_uri", "http://localhost:5173/callback");
     
@@ -100,11 +122,11 @@ async function generateCodeChallenge(codeVerifier) {
 }
 
 // Verifies POST request, returns access token
-export async function getAccessToken(clientId, code) {
+export async function getAccessToken(user, code) {
     const verifier = localStorage.getItem("verifier");
 
     const params = new URLSearchParams();
-    params.append("client_id", clientId);
+    params.append("client_id", user);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
     params.append("redirect_uri", "http://localhost:5173/callback");
@@ -138,9 +160,9 @@ async function fetchTop(token) {
     return await result.json();
 }
 
-// Searches for a song given the genre
-async function fetchSong(token) {
-    const result = await fetch("https://api.spotify.com/v1/search?q=tag%3Ahipster+genre%3Aindie+pop&type=track&market=US&limit=10", {
+// Searches for a song given genre
+async function fetchSong(token, request) {
+    const result = await fetch(request, {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
 
@@ -161,7 +183,6 @@ function populateUI(profile) {
         document.getElementById("imgUrl").innerText = profile.images[0].url;
     }
     document.getElementById("id").innerText = profile.id;
-    document.getElementById("email").innerText = profile.email;
     document.getElementById("uri").innerText = profile.uri;
     document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
     document.getElementById("url").innerText = profile.href;
